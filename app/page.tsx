1d1dea4dfee34f427dee964e9f4c72412c190641
LuigiRechine <lbarrosrechinelli@gmail.com>
2026-04-02 16:10:12 -0300

    primeiro-next

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,21 @@
-import Image from "next/image";
+import "./home.css";
 
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+export default function Home(){
+  return(
+    <div className="cont">
+      <h1 className="title">Minhas férias</h1>
+      <div className="part">
+        <p className="txt">Minhas férias foram bem tranquilas, passei a maior parte do tempo em casa descansando e aproveitando para relaxar depois do ano de estudos. Mesmo sem fazer muitas viagens, consegui aproveitar esse período para descansar bastante.</p>
+        <img src="https://s2-oglobo.glbimg.com/z35OK2v4E62JbijpeECdYmDktXs=/0x0:6048x4032/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_da025474c0c44edd99332dddb09cabe8/internal_photos/bs/2025/8/y/q8wjANRhASNauJBrbtUw/desta-vez-e-definitivamente-muito-cedo-para-mim.jpg" alt="" />
+      </div>
+      <div className="part">
+        <img src="https://turismodenatureza.com.br/wp-content/uploads/2024/10/Complexo-do-Claro-Delfinopolis.jpg" alt="" />
+        <p className="txt">Na virada do ano, viajei com minha família para Delfinópolis, em Minas Gerais. Lá foi muito legal, porque pude sair um pouco da rotina, curtir a natureza e passar um tempo diferente com todo mundo.</p>
+      </div>
+      <div className="part">
+        <p>No geral, minhas férias foram simples, mas boas. Mesmo ficando bastante em casa, consegui descansar e aproveitar momentos importantes com a família.</p>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/d/df/Family_Portrait.jpg" alt="" />
+      </div> 
     </div>
-  );
+  )
 }
